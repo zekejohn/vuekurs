2b2ee4c8bf4af29af421cd20b1b7bd302eea345b
John Eirik Bjorhovde Nilsen <john.eirik.bjorhovde.nilsen@visma.com>
2019-11-05 12:30:08 +0100

    readme

---
+++ README.md
@@ -0,0 +1,37 @@
+# vue-kurs
+
+
+Forberedelser
+
+## NODE 
+
+https://nodejs.org/en/
+
+## VUE 
+```
+$ npm install vue 
+```
+
+## VUE UI:
+```
+$ npm install -g @vue/cli
+```
+
+## VUE Plug-in browsers
+https://chrome.google.com/webstore/detail/vuejs-devtools/ljjemllljcmogpfapbkkighbhhppjdbg
+
+## IDE
+Valgfritt (anbefaler VSCode)
+
+
+Klon repoet med git, evt last ned repoet med den grønne knappen oppe til høyre og unzip.
+
+## Installer prosjekt fra cmd med cd vue-kurs
+```
+npm install
+```
+
+### Kjør utviklingsserver
+```
+npm run serve
+```
--- vue-kurs/src/App.vue
+++ vue-kurs/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/exercise1">Exercise 1</router-link> |
-      <router-link to="/exercise2">Exercise 2</router-link> |
-      <router-link to="/exercise3">Exercise 3</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/exercise1">Exercise 1</router-link>|
+      <router-link to="/exercise2">Exercise 2</router-link>|
+      <router-link to="/exercise3">Exercise 3</router-link>|
+      <router-link to="/exercise4">Exercise 4</router-link>|
+      <router-link to="/exercise5">Exercise 5</router-link>|
+      <router-link to="/exercise6">Exercise 6</router-link>
     </nav>
     <router-view></router-view>
   </div>
@@ -12,13 +15,13 @@
 
 <script>
 export default {
-  name: 'app',
-}
+  name: "app"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
