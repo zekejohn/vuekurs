ce8f94af6e779aff49087e050a4d46acb3960f7f
zekejohn <johneiriknilsen@gmail.com>
2019-11-05 10:52:37 +0100

    Exercise 4 og 6

--- vue-kurs/src/components/Exercise4.vue
+++ vue-kurs/src/components/Exercise4.vue
@@ -1,18 +1,81 @@
 <template>
   <div>
     <h1>{{header}}</h1>
-    <p>{{count}}</p>
-    <button @click="increment">Increment</button>
+
+    <select v-model="selected">
+      <option value="">--Velg favorittlag i eliteserien--</option>
+      <option v-for="(option, index) in options" v-bind:value="option.value" :key="index">
+        {{ option.text }}
+      </option>
+    </select>
+    <div class="wrapper" style="padding: 40px;">
+      <keep-alive>
+        <component v-bind:is="componentName"></component>
+      </keep-alive>
+    </div>
   </div>
 </template>
 
 <script>
+import Rosenborg from "./dynamic/Rosenborg";
+import Molde from "./dynamic/Molde";
+import Enga from "./dynamic/Enga";
+import Brann from "./dynamic/Brann";
+import Odd from "./dynamic/Odd";
+import Glimt from "./dynamic/Glimt";
+import Other from "./dynamic/Other";
+import Nobody from "./dynamic/Nobody";
 export default {
   name: 'Exercise4',
-  data: function () {
+    components: {Nobody, Other, Glimt, Odd, Brann, Enga, Molde, Rosenborg},
+    data: function () {
       return {
         header: "Exercise 4",
-        count: 0
+        count: 0,
+        selected: '',
+        options: [
+            { text: 'Ingen', value: 'nobody' },
+            { text: 'Rbk', value: 'rbk' },
+            { text: 'Molde', value: 'mfk' },
+            { text: 'Glimt', value: 'glimt' },
+            { text: 'Odd', value: 'odd' },
+            { text: 'Brann', value: 'brann'},
+            { text: 'Vålerenga', value: 'enga' },
+            { text: 'Andre', value: 'other'},
+        ]
+      }
+  },
+  computed: {
+      componentName() {
+          let componentName = null;
+          switch (this.selected) {
+              case 'rbk':
+                  componentName = Rosenborg.name;
+                  break;
+              case 'mfk':
+                  componentName = Molde.name;
+                  break;
+              case 'enga':
+                  componentName = Enga.name;
+                  break;
+              case 'brann':
+                  componentName = Brann.name;
+                  break;
+              case 'odd':
+                  componentName = Odd.name;
+                  break;
+              case 'glimt':
+                  componentName = Glimt.name;
+                  break;
+              case 'other':
+                  componentName = Other.name;
+                  break;
+              case 'nobody':
+                  componentName = Nobody.name;
+                  break;
+              default:
+          }
+          return componentName;
       }
   },
   methods: {
@@ -22,3 +85,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+</style>
--- vue-kurs/src/components/Exercise6.vue
+++ vue-kurs/src/components/Exercise6.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
     <h1>{{header}}</h1>
-    <p>{{count}}</p>
-    <button @click="increment">Increment</button>
+    <div class="wrapper">
+      <div style="padding:40px;">
+        <input type="text" v-model="color">
+      </div>
+      <div class="circle" :style="'background:' + invertColor(color)">
+        <div align="center">
+          <div class="yin" :style="'background:' + color"></div>
+        </div>
+        <div align="right">
+          <div class="feher" :style="'background:' + color"></div>
+        </div>
+        <div align="center">
+          <div class="yang" :style="'background:' + invertColor(color)"></div>
+        </div>
+
+        <div align="center">
+          <div class="p-black" :style="'background:' + invertColor(color)"></div>
+          <div class="p-white" :style="'background:' + color"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,13 +31,108 @@ export default {
   data: function () {
       return {
         header: "Exercise 6",
-        count: 0
+        count: 0,
+        color: '#FFFFFF'
       }
   },
   methods: {
-      increment(){
-          this.count += 1;
+      invertColor(hex) {
+          if (hex.indexOf('#') === 0) {
+          hex = hex.slice(1);
+          }
+          // convert 3-digit hex to 6-digits.
+          if (hex.length === 3) {
+              hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+          }
+          if (hex.length !== 6) {
+              throw new Error('Invalid HEX color.');
+          }
+          // invert color components
+          var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+              g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+              b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+          // pad each with zeros and return
+          return '#' + this.padZero(r) + this.padZero(g) + this.padZero(b);
+      },
+
+      padZero(str, len) {
+          len = len || 2;
+          var zeros = new Array(len).join('0');
+          return (zeros + str).slice(-len);
       }
   }
 }
 </script>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .box {
+    max-width: 50%;
+    width: 200px;
+    height: 200px;
+    border: 1px solid black;
+  }
+
+  .circle {
+    width:300px;
+    height:300px;
+    background:black;
+    border-radius:100%;
+    text-align:center;
+    animation:rotate 40000s infinite;
+  }
+  .circle .yin {
+    width:150px;
+    height:150px;
+    background:white;
+    border-radius:100%;
+  }
+
+  .circle .yang {
+    width:150px;
+    height:150px;
+    background:black;
+    border-radius:100%;
+    margin-top:-154px;
+    z-index:auto;
+    position: absolute;
+    margin-left:75px;
+
+  }
+
+  .feher {
+    width:150px;
+    height:300px;
+    background:white;
+    display:inline-block;
+    margin-top:-150px;
+    border-top-right-radius:150px;
+    border-bottom-right-radius:150px;
+  }
+
+  .p-black {
+    width:50px;
+    height:50px;
+    background:black;
+    border-radius:100%;
+    position:absolute;
+    margin-top:-260px;
+    margin-left:120px;
+  }
+
+  .p-white {
+    width:50px;
+    height:50px;
+    background:white;
+    border-radius:100%;
+    position:absolute;
+    margin-top:-110px;
+    margin-left:120px;
+  }
+</style>
